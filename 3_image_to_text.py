59490cafceb6597bbb2188cb371dfa42662086e3
Priyadarshi Soumya Kumar <priyadarshisoumyakumar@macbookpro.mynetworksettings.com>
2024-11-30 15:46:28 -0500

    added more photos

--- 3_image_to_text.py
+++ 3_image_to_text.py
@@ -16,14 +16,20 @@ image = httpx.get(image_path)
 
 image_path = '/Users/priyadarshisoumyakumar/Downloads/LLMVSCode/BasicLLM/test_img.jpeg'
 sample_file_1 = PIL.Image.open(image_path)
-
 prompt = "Caption this image."
 answer = model.generate_content([{'mime_type':'image/jpeg', 'data': base64.b64encode(image.content).decode('utf-8')}, prompt])
-
 prompt = "Write whats happening in the image"
-
 response = model.generate_content([prompt, sample_file_1])
+
+
+food_img_path = "/Users/priyadarshisoumyakumar/Downloads/LLMVSCode/BasicLLM/somefood.jpg"
+food_img = PIL.Image.open(food_img_path)
+food_img_promt = "Tell me ingredients of this food image and its geogriphical culture"
+food_img_response = model.generate_content([food_img_promt,food_img])
+# model.generate_content([{'mime_type':'image/jpeg', 'data': base64.b64encode(food_img_promt.content).decode('utf-8')}, food_img_promt])
 
 print(answer.text)
 print("****************************************")
 print(response.text)
+print("******************************************")
+print(food_img_response.text)
